95fec158d09cc7c2af6ea120158e59591c5e9106
Наташа Неровня <natanerovnja@gmail.com>
2016-03-20 10:08:07 +0300

    Add console test for converting to heximal system

---
+++ IntExtensionTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntExtension;
+
+namespace IntExtensionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"-12235 = {(-12235).ConvertToHex()}");
+            Console.WriteLine($"73243 = {(73243).ConvertToHex()}");
+            Console.WriteLine($"16 = {(16).ConvertToHex()}");
+            Console.WriteLine($"15 = {(15).ConvertToHex()}");
+
+            Console.Read();
+        }
+    }
+}
